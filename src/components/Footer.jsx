28b5c32f999a4083e5a7280dd476e9d931fe8235
Samaro <samaro.dev@gmail.com>
2025-08-20 16:18:37 +0100

    Upload React App project #01

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const year = new Date().getFullYear();
+console.log(year);
+
+function Footer() {
+  return (
+    <footer>
+      <p>@ Copyright {year}</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note( { content } ) {
+  return (
+    <div className="note">
+      <h1>Hello</h1>
+      <p>{content}</p>
+    </div>
+  );
+}
+
+export default Note;
