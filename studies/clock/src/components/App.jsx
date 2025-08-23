f772245a80fa8a998a687c51306a7bb449bb448c
Gonçalo Amaro <samaro.dev@gmail.com>
2025-08-23 21:12:13 +0100

    Add setInt comment to clock.studies

--- studies/clock/src/components/App.jsx
+++ studies/clock/src/components/App.jsx
@@ -1,24 +1,25 @@
 import React, { useState } from "react";
 
 function App() {
-  let now = new Date().toLocaleTimeString();
+  let now = new Date(2025, 1, 1, 12).toLocaleTimeString();
+  console.log(now);
 
   const [time, setTime] = useState(now);
-  console.log(time);
 
   function refreshTime() {
-    let newTime = new Date().toLocaleTimeString();
-    setTime(newTime);
+    let newNow = new Date().toLocaleTimeString();
+    setTime(newNow);
   }
 
-  setInterval(refreshTime, 1000);
+  setInterval(refreshTime, 1000); // <----- creates a simulation of a real clock
 
   return (
     <div className="container">
       <h1>{time}</h1>
-      <button onClick={refreshTime}>Get Time</button>
+      <button onClick={refreshTime}>GET TIME</button>
     </div>
   );
 }
 
 export default App;
+
